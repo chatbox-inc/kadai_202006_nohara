358fad34bb73cb295196989b6866258accdf1c38
野原晃司 <nohara.koji.infratop@gmail.com>
2020-06-25 13:45:29 +0900

    finish layout#2

---
+++ pages/index.vue
@@ -0,0 +1,509 @@
+<template>
+    <div>
+	    <header>
+	    	<!-- positionで指定できる -->
+	    </header>
+        <section>
+        	<div class="fv-txt">
+			    <!-- 画像挿入 -->
+				<p class="fv-txt__txt pc-only">
+				    ここは  福岡県うきは市の  ぶどうの産地
+				    <br>
+				    “ 僕らはまだまだ「たね」だから、がんばっていかないと ”
+				    <br>
+				    やがて成熟して実となり
+				    葉っぱが芽吹くように  時は流れる
+				    <br>
+				    ようこそ  暮らしのものがたりへ
+				</p>
+	            <p class="fv-txt__txt sp-only">
+	            	ここは  福岡県うきは市の
+	            	<br>
+	            	ぶどうの産地
+	            	<br>
+	            	<br>
+	            	やがて成熟して実となり
+	            	<br>
+	            	葉っぱが芽吹くように
+	            	<br>
+	            	時は流れる
+	            	<br>
+	            	<br>
+	            	ようこそ
+	            	<br>
+	            	暮らしのものがたりへ
+	            </p>
+	        </div>
+	        <div class="fv-news">
+	        	<a class="fv-news__single" href="">
+	        	    <div class="fv-news__txt">
+		        		<div class="fv-news__date">2020/06/13</div>
+		        		<div class="fv-news__ttl">ぶどうのたね　カフェスタッフ募集のお知らせ</div>
+		        	</div>
+	        	</a>
+	        	<a class="fv-news__more" href="">
+	        		<!-- 矢印の表示の方法わからん -->
+	        		<span>more</span>
+	        	</a>
+	        </div>
+		</section>
+		<div class="shop">
+			<h2 class="c-heading">BUDOU NO TAKE</h2>
+            <div class="shop-box is-inview shop-box__0 is-enter">
+            	<a class="shop-box__meta shop-box__0_meta" href="">
+				    <div class="shop-box__tag">
+				    	<div class="shop-box__name">
+				    		<!--　写真挿入 -->
+				    	</div>
+				    	<div class="shop-box__tags">
+					        <span>買う</span>
+					        <span>見る</span>
+					        <span>着る</span>
+					    </div>
+					    <div class="shop-box__txt">
+						    <p>着心地のよいものを<br>豊かな衣替え</p>
+					    </div>
+				    </div>
+				</a>
+			</div>
+            <div class="shop-box is-inview shop-box__1 is-enter">
+            	<a class="shop-box__meta shop-box__1_meta" href="">
+				    <div class="shop-box__tag">
+				    	<div class="shop-box__name">
+				    		<!--　写真挿入 -->
+				    	</div>
+				    	<div class="shop-box__tags">
+					        <span>食べる</span>
+					        <span>飲む</span>
+					        <span>落ち着く</span>
+					    </div>
+					    <div class="shop-box__txt">
+						    <p>古民家の風合いを感じながら<br>いただく隣のごはん</p>
+					    </div>
+				    </div>
+				</a>
+			</div>
+            <div class="shop-box is-inview shop-box__2 is-enter">
+            	<a class="shop-box__meta shop-box__2_meta" href="">
+				    <div class="shop-box__tag">
+				    	<div class="shop-box__name">
+				    		<!--　写真挿入 -->
+				    	</div>
+				    	<div class="shop-box__tags">
+					        <span>買う</span>
+				            <span>見る</span>
+				            <span>おみやげ</span>
+					    </div>
+					    <div class="shop-box__txt">
+						    <p>食まわりの軽やかな雑貨<br>暮らしにまつわる道具たち</p>
+					    </div>
+				    </div>
+				</a>
+			</div>
+            <div class="shop-box is-inview shop-box__3 is-enter">
+            	<a class="shop-box__meta shop-box__3_meta" href="">
+				    <div class="shop-box__tag">
+				    	<div class="shop-box__name">
+				    		<!--　写真挿入 -->
+				    	</div>
+				    	<div class="shop-box__tags">
+					        <span>買う</span>
+				        	<span>食べる</span>
+				        	<span>おみやげ</span>
+					    </div>
+					    <div class="shop-box__txt">
+						    <p>贈り物にもぴったりな最中菓子<br>「豆たん」をおひとつどうぞ</p>
+					    </div>
+				    </div>
+				</a>
+			</div>
+            <div class="shop-box is-inview shop-box__4 is-enter">
+            	<a class="shop-box__meta shop-box__4_meta" href="">
+				    <div class="shop-box__tag">
+				    	<div class="shop-box__name">
+				    		<!--　写真挿入 -->
+				    	</div>
+				    	<div class="shop-box__tags">
+					        <span>買う</span>
+				            <span>見る</span>
+				            <span>飾る</span>
+					    </div>
+					    <div class="shop-box__txt">
+						    <p>器と出会う喜び<br>新作はSTOREにて</p>
+					    </div>
+				    </div>
+				</a>
+			</div>
+            <div class="shop-box is-inview shop-box__5 is-enter">
+            	<a class="shop-box__meta shop-box__5_meta" href="">
+				    <div class="shop-box__tag">
+				    	<div class="shop-box__name">
+				    		<!--　写真挿入 -->
+				    	</div>
+				    	<div class="shop-box__tags">
+					        <span>買う</span>
+				            <span>見る</span>
+				            <span>姿勢を正す</span>
+					    </div>
+					    <div class="shop-box__txt">
+						    <p>着物を着た人の存在感<br>美しい暮らしが残る場所</p>
+					    </div>
+				    </div>
+				</a>
+			</div>
+            <div class="shop-box is-inview shop-box__6 is-enter">
+            	<a class="shop-box__meta shop-box__6_meta" href="">
+				    <div class="shop-box__tag">
+				    	<div class="shop-box__name">
+				    		<!--　写真挿入 -->
+				    	</div>
+				    	<div class="shop-box__tags">
+					        <span>買う</span>
+				            <span>飲む</span>
+				            <span>くつろぐ</span>
+					    </div>
+					    <div class="shop-box__txt">
+						    <p>気候と風土が調和したコーヒー<br>豆売りも行っております</p>
+					    </div>
+				    </div>
+				</a>
+			</div>
+            <div class="shop-box is-inview shop-box__7 is-enter">
+            	<a class="shop-box__meta shop-box__7_meta" href="">
+				    <div class="shop-box__tag">
+				    	<div class="shop-box__name">
+				    		<!--　写真挿入 -->
+				    	</div>
+				    	<div class="shop-box__tags">
+					        <span>買う</span>
+				            <span>食べる</span>
+				            <span>飲む</span>
+		                    <span>落ち着く</span>
+					    </div>
+					    <div class="shop-box__txt">
+						    <p>彩豊かなうきは産の旬の食材<br>ゆったりとしたお食事を</p>
+					    </div>
+				    </div>
+				</a>
+			</div>
+	    </div>
+	    <footer>
+	    </footer>
+	    <div class="recruit">
+	    	<h2 class="recruit-heading">RECRUIT</h2>
+	        <p class="recruit-desc">
+		        ぶどうのたね採用ページへようこそ。
+		        <br>
+				福岡県の南部うきは市にぶどうのたねはあります。
+				<br>
+				大分県との県境、山に囲まれた街です。
+				<br>
+				近くには阿蘇山系からの川がたくさん点在しています。
+				<br>
+				自然豊かなうきは市では年間を通してたくさんの野菜や果物実ります。
+				<br>
+				そしてぶどうのたねの上には見守り山「五葉山」、見守り神社「諏訪宮」。
+				<br>
+				守られ、緑に囲まれたこの土地に私たちの「場所」があります。
+				<br>
+				<br>
+				「土地ありき」「人ありき」私たちが大切にしている理念です。
+				<br>
+				<br>
+				土地ありき。自然は私たちの力では作ることはできません。
+				<br>
+				できることは、守っていくことです。
+				<br>
+				私たちも仕事をしながら、気持ちよく、風通しのいい場所にしたいと思っています。
+				<br>
+				朝は店舗と庭掃除から始まります。
+				<br>
+				掃除をしているとつい下ばかりを向いてしまいますが、鳥の鳴き声や心地よい風が吹くと、空を見あげます。ながれる雲や山々の緑が目にはいると、気持ちがリセッ
+				トされます。
+				<br>
+				自然に感謝する瞬間です。
+				<br>
+				<br>
+				人ありき。この「場所」は今まで関わった全ての方たちとともに守って来ました。
+				<br>
+				器、服飾のギャラリー、カフェ、和菓子屋、自家焙煎コーヒー、そして着物。
+				<br>
+				ぶどうのたねのスタッフは幅広い世代と、経験も様々。個性豊かなのスタッフがいます。
+				<br>
+				お互いを尊重し、毎日の仕事を作業としてこなすのではなく、多方面での学びを感じていただける方を希望いたします。
+				<br>
+				<br>
+				<br>
+				今回はカフェのキッチンとホールスタッフを募集です。
+				<br>
+				<br>
+				<br>
+				求める人物像
+				<br>
+				・ぶどうのたねに興味がある方。
+				<br>
+				・心身ともに健康で協調性のある方。
+				<br>
+				・料理のことが好きで食材や使用してるうつわや道具を大切に
+				<br>
+				　取り扱うことができる方。
+				<br>
+				・目の前のことだけではなく、周りに目を配れる方。
+				<br>
+				・何事も前向きに取り組める方。
+				<br>
+				・素直に話が聞ける方。
+				<br>
+				・思いやりのある方。
+				<br>
+				<br>
+				<br>
+				この地を守り、お互いを大切に思い、私たちと一緒にお客様へ心地よい時間と豊かになれる気持ちを伝え届けられる方ご応募お待ちしております。
+				<br>
+				<br>
+			</p>
+			<div class="recruit-content">
+			    <div class="recruit-content__box">
+			        <div class="recruit-content__heading h1">募集職種</div>
+			        <div class="recruit-content__content c1">・キッチンスタッフ<br>・ホールスタッフ
+			        </div>
+			    </div>
+			    <div class="recruit-content__box">
+			        <div class="recruit-content__heading h2">
+			        	勤務地
+			        </div>
+			        <div class="recruit-content__content c2">
+			        	勤務地
+			        	<br>
+			        	ぶどうのたね内カフェ
+			        	<br>
+			        	福岡県うきは市浮羽町流川428
+			        </div>
+			    </div>
+			    <div class="recruit-content__box">
+			        <div class="recruit-content__heading h3">
+			      	    仕事内容
+			        </div>
+			        <div class="recruit-content__content c3">
+				      	仕事内容
+				      	<br>
+				  		(1) キッチンスタッフ
+				  		<br>
+						・野菜を中心としたランチや地元の果物を使用したスイーツの調理・仕込み。
+						<br>
+						・メニュー、レシピ開発
+						<br>
+						・キッチンで使用する食材や資材の管理や発注
+						<br>
+						(2) ホールスタッフ
+						<br>
+						・カフェレストランのホールでの接客やキッチン業務のサポート
+						<br>
+						・カフェで使用する食材や資材の管理や発注。
+						<br>
+						・電話対応
+						<br>
+						・ＳＮＳによるお店の情報発信
+				    </div>
+			    </div>
+			    <div class="recruit-content__box">
+			        <div class="recruit-content__heading h4">連絡先</div>
+			        <div class="recruit-content__content c4">
+				        <p class="sp-only">
+				        	履歴書送付先
+				        	<br>
+				        	〒839-1411
+				        	<br>
+				        	福岡県うきは市浮羽町流川334
+				        	<br>
+				        	株式会社　田中屋
+				        	<br>
+				        	採用担当　田中 あかね 宛
+				        	<br>
+			            </p>
+				        <p class="pc-only">
+				        	履歴書送付先
+				        	<br>
+				        	〒839-1411
+				        	<br>
+				        	福岡県うきは市浮羽町流川334
+				        	<br>
+				        	株式会社　田中屋
+				        	<br>
+				        	採用担当　田中 あかね 宛
+				        	<br>
+				        </p>
+                    </div>
+                </div>
+  	        </div>
+  	    </div>
+	</div>
+</template>
+
+<script>
+export default {
+  }
+</script>
+
+<style>
+    section {
+    	height: 100vh;
+    }
+    .fv-txt{
+    	position: absolute;
+    	bottom: 40vw;
+    	left: 5vw;
+    }
+    .fv-txt__txt{
+    	font-size: 3.4375vw;
+    	letter-spacing: 0.12em;
+    	line-height: 1.75;
+
+    }
+    .pc-only {
+    	display: none;
+    }
+    .fv-news {
+    	position: absolute;
+    	bottom: 3.75vw;
+    	right: 2.1875vw;
+    	padding: 4.0625vw 2.8125vw 4.0625vw 4.0625vw;
+    	background-color: #fff;
+
+        }
+    .c-heading{
+    	text-align: center;
+    	letter-spacing: .4em;
+    }
+    .shop-box {
+    	position: relative;
+    }
+    .shop-box__0 {
+    	height: 98.75vw;
+    	margin-top: 50.3125vw;
+
+    }
+    .shop-box__0_meta {
+        left: 5vw;
+
+    }
+    .shop-box__1 {
+    	height: 100.9375vw;
+    	margin-top: 5.3125vw;
+
+    }
+    .shop-box__1_meta {
+        right: 5vw;
+
+    }
+    .shop-box__2 {
+    	height: 100vw;
+    	margin-top: 6.25vw;
+
+    }
+    .shop-box__2_meta {
+        left: 5vw;
+
+    }
+    .shop-box__3 {
+    	height: 104.375vw;
+    	margin-top: 7.1875vw;
+
+    }
+    .shop-box__3_meta {
+        right: 5vw;
+
+    }
+    .shop-box__4 {
+    	height: 98.75vw;
+    	margin-top: 3.75vw;
+
+    }
+    .shop-box__4_meta {
+        left: 5vw;
+
+    }
+    .shop-box__5 {
+    	height: 104.6875vw;
+    	margin-top: 5.3125vw;
+
+    }
+    .shop-box__5_meta {
+        right: 5vw;
+
+    }
+    .shop-box__6 {
+    	height: 92.5vw;
+    	margin-top: 5.625vw;
+
+    }
+    .shop-box__6_meta {
+        left: 5vw;
+
+    }
+    .shop-box__7 {
+    	height: 101.875vw;
+    	margin-top: 7.5vw;
+
+    }
+    .shop-box__7_meta {
+        right: 5vw;
+
+    }
+
+    .shop-box__meta {
+        position: absolute;
+        display: inline-flex;
+        flex-direction: column;
+    	bottom: 0;
+    }
+    .shop-box__tags span {
+    	border: 1px #1b1b1b solid;
+    	color: #1b1b1b;
+    	font-size: 3.4375vw;
+    	display: inline-block;
+    	padding: 0 3.4375vw;
+    	line-height: 7.187vw;
+    	border-radius: .9375vw;
+    	margin-right: 1.5625vw;
+
+    }
+
+    .recruit {
+    	margin-left: auto;
+    	margin-right: auto;
+    	max-width: 90vw;
+    	padding: 12.1875vw 0;
+    }
+    .recruit-heading {
+    	text-align: center;
+    	letter-spacing:  .4em;
+    	margin-bottom: 6.25vw;
+    }
+    .recruit-desc {
+    	line-height: 1.8181818182;
+    	margin-bottom: 6.25vw;
+    	text-align: justify;
+    }
+    .recruit-content {
+    	border-bottom: 1px #b58a63 solid;
+    	display: flex;
+    	flex-direction: column;
+    }
+@media(min-width: 768px){
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
