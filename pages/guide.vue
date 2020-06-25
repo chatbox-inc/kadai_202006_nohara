f9917e6ae8dd73fb9d1628bc11224c893d82548a
野原晃司 <nohara.koji.infratop@gmail.com>
2020-06-25 16:56:45 +0900

    finish #3

---
+++ pages/guide.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class="c-wrapper" id="top">
+		<div class="c-inner">
+		  	<h2 class="c-inner__heading" data-txt="guide">
+			  サイトのご利用にあたって
+			</h2>
+			<div class="c-inner__body">
+				<h2>ご利用ブラウザについて
+				</h2>
+				<p>「ぶどうのたね」のウェブサイトをご覧になる際は、以下の環境でご覧いただくことを推奨します。推奨環境以外でご利用いただいた場合や､推奨環境下でもお客さまのブラウザの設定等によっては、正しく表示されない場合がありますのでご了承ください｡ また､CookieおよびJavaScriptが有効でない場合は､一部のサイトを正しく見ることができません。
+				</p>
+				<h3>PC 推奨OS / ブラウザ</h3>
+				<p>【 Windows 】<br>Microsoft Internet Explorer 11 , Google Chrome 最新版 , Mozilla Firefox 最新版
+				</p>
+				<p>【 Macintosh 】<br>Safari 最新版 , Google Chrome 最新版 , Mozilla Firefox 最新版<br><small>※ブラウザの自動アップデート機能が有効になっていない場合は、最新版へのアップデートを推奨します。</small>
+				</p>
+				<h3>スマートフォン 推奨OS / ブラウザ</h3>
+				<p>【 iPhone 】<br>iOS 10 以上 / Chrome、Safari のそれぞれの最新版</p>
+				<p>【 Android 】<br>Android 6.0.1 以上 / 標準ブラウザ、Chrome、Safari のそれぞれの最新版</p>
+				<h2>セキュリティについて</h2>
+				<p>本サイトでは、お客さまの個人情報を「SSL」により保護しております。SSLを使うためにブラウザに特別な設定をする必要はございません。<br>セキュリティ機能に対応したブラウザを使用することで、お客さまが入力される本名や住所あるいは電話番号などの個人情報を自動的に暗号化して送受信します。<br>万が一、送信データを第三者が傍受した場合でも、内容が盗み取られる心配はございません。<br>SSLによるセキュア通信をしているページには、ブラウザの下部に施錠された鍵マークが表示されます。<br><small>※SSL（Secure Sockets Layer）インターネット上で、情報を暗号化して通信するセキュリティ機能</small>
+				</p>
+		    </div>
+	    </div>
+		<div class="c-pictmap">
+			<!-- マップ画像 -->
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+    .c-wrapper {
+    	position: relative;
+    }
+    .c-inner {
+    	margin-left: auto;
+    	margin-right: auto;
+    	padding: 45vw 0 20vw;
+    	max-width: 90vw;
+    }
+    .c-inner__heading {
+    	text-align: center;
+    	letter-spacing: .16em;
+    	margin-bottom: 24.375;
+    }
+    .c-inner__body {
+    	padding: 0 5vw;
+    }
+
+</style>
